566e2c7dab1281e706bcf41d347f1bbc505f6fbe
ahmadalrasheed <alrasheedahmad97@gmail.com>
2021-08-18 18:38:34 +0300

    finishing lab8 with added movies

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ server.use(cors());
 
 const PORT=process.env.PORT;
 
-// http://localhost:3001/weatherdata?city_name=Seattle
 // console.log('helloooo');
 
 server.listen(PORT,()=>{
@@ -29,6 +28,10 @@ server.get('/',(req,res)=>{
 });
 
 
+
+
+// getting info from Weather API 
+// http://localhost:3001/weatherdata?city_name=Seattle
 
 server.get('/weatherdata',async (req,res)=>{
   try{
@@ -52,6 +55,30 @@ server.get('/weatherdata',async (req,res)=>{
 
 });
 
+
+
+// getting information from MOVIE API
+// http://localhost:3001/movie?movieSearch=tokyo
+
+server.get('/movie',async (req,res)=>{
+  try{
+    let movieSearch=req.query.movieSearch
+    let movieData=await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${movieSearch}`)
+    
+    let MoviesList=movieData.data.results
+    let CityMovies=MoviesList.map(Movies=>{
+      let AddMovieList = new MovieList(Movies.original_title,`Overview :${Movies.overview}`,`Average Votes :${Movies.vote_average}`,`Total Votes :${Movies.vote_count}`,Movies.poster_path,`Popularity :${Movies.popularity}`,`Release Date :${Movies.release_date}`)
+      return AddMovieList
+    })
+    res.send(CityMovies);
+  }
+  catch(error){
+    console.log(error);
+    res.send('ERROR')
+  }
+
+})
+
 server.get('*',(req,res)=>{
   res.status(500).send('ERROR: NOT EXPECTED URL');
 });
@@ -60,5 +87,18 @@ class Forecast {
   constructor(date, description) {
     this.date = date;
     this.description = description;
+  }
+}
+
+class MovieList{
+  constructor(title,overview,average_votes,total_votes,poster_path,popularity,released_on){
+    this.title=title;
+    this.overview=overview;
+    this.average_votes=average_votes;
+    this.total_votes=total_votes;
+    this.image_url=`https://image.tmdb.org/t/p/w500${poster_path}`
+    this.popularity=popularity;
+    this.released_on=released_on;
+
   }
 }
